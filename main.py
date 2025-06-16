a07328350a1ca08a21b9df91c11dad2875c4a447
max <bruno.m.voss@gmail.com>
2025-06-16 10:40:20 +0200

    basics work

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 import os
 import uuid
+import random
 
 class Question:
     """different levels of questions, that unlock after prerequisites?"""
@@ -16,15 +17,20 @@ class Question:
         else:
             self.answers = []
         self.q_uuid=str(uuid.uuid4())
+        self.correct_count = 0
         
 class LearningSystem:
     def __init__(self):
-        self.try_load_questions()
+        
+        
+        # maybe I want different levels?
         
         self.questions = {}
         self.active_question_ids = []
         self.solved_questions = []
-        # maybe I want different levels?
+        
+        self.try_load_questions()
+        
         
         
     def try_load_questions(self):
@@ -41,22 +47,30 @@ class LearningSystem:
             current_question = None
             for line in t:
                 line=line.split(";")
-                myid, questiontext, answer_text=line
-                if myid != currentid:
+                if len(line)==1:
+                    break
+                myid, question_text, answer_text=line
+                if myid != current_id:
                     if current_question!=None:
                         self.questions[current_question.q_uuid]=current_question
+                    print(f"making {question_text}")
                     current_question = Question(question_text)
                     
                     if myid!="":
                         current_question.q_uuid = myid
                         
                     current_id = current_question.q_uuid
-                    current_question.answers.append(answer_text)
-                    
+                current_question.answers.append(answer_text)
+            
+            #add the last
+            self.questions[current_question.q_uuid]=current_question
             
         else:
             print(f"no {questions_fn} found")
-            
+        
+        print("loaded", len(self.questions), "questions")
+        
+        
         if current_questions_fn in myfile_list:
             with open(current_questions_fn,"r") as f:
                 t=f.read()
@@ -68,6 +82,7 @@ class LearningSystem:
        
     def main(self):
         self.question_management()
+        self.present_question()
         
     def present_question(self):
         #pick a ranom question
@@ -76,22 +91,29 @@ class LearningSystem:
         print(my_question.question_text)
         
         answers = list(my_question.answers)
+        print(answers)
         random.shuffle(answers)
         
-        correct_key=answers.index(my_question.answers[0])
+        correct_key = str(answers.index(my_question.answers[0]))
+        print("correct_key",[correct_key])
         c = 0
-        answer_keys=[]
+        answer_keys = []
         for x in answers:
             print(f"{c}): {x}")
-            answer_keys.append(c)
+            answer_keys.append(str(c))
             c += 1
         
-        a = input("your answer")
-        if a in answer_keys:
-            if a == correct_key:
+        user_answer = input("your answer")
+        print([user_answer])
+        if user_answer in answer_keys:
+            if user_answer == correct_key:
                 # up the count or retire
+                print("correct")
+                my_question.correct_count += 1
             else:
+                print("wrong")
                 # reset the count
+                my_question.correct_count = 0
         else:
             print("invalid input")
         
@@ -108,6 +130,10 @@ class LearningSystem:
             self.active_question_ids.append(this)
             
     def pick_questions(self):
+        
+        # I want a high frequency and a low frequency question pool
+        # I want to do high frequency questions until none are left
+        # 
         a = 1
         
 def entry():
@@ -117,4 +143,5 @@ def entry():
     
 
 if __name__=="__main__":
+    entry()
     
